7dbf68dcde7bc82f6c4d5cc86b061bba281b91cb
Beatriz Gonçalves de Souza <102838758+Beatrizgs1@users.noreply.github.com>
2026-08-27 18:18:02 -0300

    Criar MainActivity

---
+++ app/app/src/main/app/src/main/java/com/example/appcontador/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.appcontador
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            MaterialTheme {
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ContadorScreen()
+                }
+            }
+        }
+    }
+}
